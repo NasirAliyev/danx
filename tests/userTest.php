d09619cd592bfdede419c851cce65a3bd0e9e6b8
Nasir <integer255@gmail.com>
2017-11-15 20:28:13 +0400

    updates

---
+++ tests/userTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nasir
+ * Date: 11/15/2017
+ * Time: 7:51 PM
+ */
+
+
+class usertest extends PHPUnit_Framework_TestCase
+{
+    public function testMethod1()
+    {
+        $this->assertEquals(1,1);
+    }
+}
