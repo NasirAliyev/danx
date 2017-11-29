e34286d34bc527cbb5bd597da997d440d2bc429e
Nasir <integer255@gmail.com>
2017-11-29 16:37:51 +0400

    updates

--- danx.sql
+++ danx.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2017 at 01:02 AM
+-- Generation Time: Nov 29, 2017 at 03:36 PM
 -- Server version: 5.5.48
 -- PHP Version: 5.6.19
 
@@ -40,6 +40,88 @@ CREATE TABLE IF NOT EXISTS `files` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `regions`
+--
+
+CREATE TABLE IF NOT EXISTS `regions` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=98 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `regions`
+--
+
+INSERT INTO `regions` (`id`, `name`) VALUES
+(1, 'Göygöl'),
+(2, 'Masallı'),
+(3, 'Zaqatala'),
+(4, 'Balakən'),
+(5, 'Lerik'),
+(6, 'Astara'),
+(7, 'Lənkəran'),
+(8, 'Ağsu'),
+(9, 'Gəncə'),
+(10, 'Yardımlı'),
+(11, 'Şəki'),
+(12, 'Şamaxı'),
+(13, 'Naftalan'),
+(14, 'Xocalı'),
+(15, 'Samux'),
+(16, 'Daşkəsən'),
+(17, 'Goranboy'),
+(18, 'Kəlbəcər'),
+(19, 'Qusar'),
+(20, 'Bakı'),
+(21, 'Hacıqabul'),
+(22, 'Saatlı'),
+(23, 'Sabirabad'),
+(24, 'Şirvan'),
+(25, 'Cəbrayıl'),
+(26, 'Ağdaş'),
+(27, 'Bərdə'),
+(28, 'Tərtər'),
+(29, 'Ağdam'),
+(30, 'Qax'),
+(31, 'Oğuz'),
+(32, 'Qəbələ'),
+(33, 'İsmayıllı'),
+(34, 'Şuşa'),
+(35, 'Abşeron'),
+(36, 'Şabran'),
+(37, 'Siyəzən'),
+(38, 'Xızı'),
+(39, 'Zəngilan'),
+(40, 'Laçın'),
+(41, 'Qubadlı'),
+(42, 'Ucar'),
+(43, 'Kürdəmir'),
+(44, 'Zərdab'),
+(45, 'Şəmkir'),
+(46, 'Gədəbəy'),
+(47, 'Tovuz'),
+(48, 'Ağstafa'),
+(49, 'Qazax'),
+(50, 'Beyləqan'),
+(51, 'Ağcabədi'),
+(52, 'İmişli'),
+(53, 'Füzuli'),
+(54, 'Xocavənd'),
+(55, 'Cəlilabad'),
+(56, 'Biləsuvar'),
+(57, 'Neftçala'),
+(58, 'Salyan'),
+(59, 'Qobustan'),
+(60, 'Mingəçevir'),
+(61, 'Quba'),
+(62, 'Xaçmaz'),
+(63, 'Yevlax'),
+(64, 'Göyçay'),
+(65, 'Sumqayıt');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -55,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `company_long` varchar(100) DEFAULT NULL,
   `address` varchar(100) DEFAULT NULL,
   `img` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -76,6 +158,7 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
   `toregion` varchar(50) DEFAULT NULL,
   `months` int(11) NOT NULL DEFAULT '1',
   `time` varchar(20) DEFAULT NULL,
+  `fromdate` varchar(10) DEFAULT NULL,
   `expire` varchar(10) DEFAULT NULL,
   `status` int(1) NOT NULL DEFAULT '1' COMMENT '1 -  gözlənilir ; 2 - baxılır  3 - dəyişiklik tələb olunur ; 4 - qəbul olunudu ; ',
   `message` text,
@@ -92,6 +175,13 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
 ALTER TABLE `files`
   ADD PRIMARY KEY (`id`),
   ADD KEY `vehicle_id` (`vehicle_id`);
+
+--
+-- Indexes for table `regions`
+--
+ALTER TABLE `regions`
+  ADD UNIQUE KEY `id_2` (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- Indexes for table `users`
@@ -116,6 +206,11 @@ ALTER TABLE `vehicles`
 --
 ALTER TABLE `files`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `regions`
+--
+ALTER TABLE `regions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=98;
 --
 -- AUTO_INCREMENT for table `users`
 --
