e515357372ad58803043ed23764c2fd98f29c5b8
Nasir <integer255@gmail.com>
2017-12-01 15:25:12 +0400

    updates

--- classes/db.php
+++ classes/db.php
@@ -12,10 +12,8 @@ class db {
 
     public static function instance()
     {
-         
         try {
-            //$conn = new PDO('mysql:host=192.168.1.3;port=3306;dbname=mhm','mhm','*#MHM5673256s',array(PDO::ATTR_PERSISTENT => true));
-            $conn =  new PDO('mysql:host=localhost;dbname=mhm1','mhm1','*#MHM5673256s');
+            $conn =  new PDO('mysql:host=localhost;dbname=danx','root','');
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $conn->exec("set names utf8");
         }
