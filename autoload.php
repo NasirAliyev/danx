943e9f3d9340891bcfc6304238114d2d0103864a
Nasir <integer255@gmail.com>
2017-12-01 15:22:21 +0400

    updates

--- autoload.php
+++ autoload.php
@@ -1,6 +1,15 @@
 <?php
-
 spl_autoload_register(function($class){
-    $class_path=__DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.basename($class).'.php';
-    if (file_exists($class_path)) require_once $class_path;
+
+    $clsStrArr=explode("\\",$class);
+    $class=end($clsStrArr);
+
+    $class_path=__DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.$class.'.php';
+
+    if (file_exists($class_path))
+        require_once $class_path;
+    else
+        die('Class not found ->  '.$class_path);
+
 });
+
